27d1767601a40af12e0ac4513ea288f2c4887aa9
zjy <jinyou.zeng@wukongsch.com>
2022-05-06 17:33:52 +0800

    https

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ import notification from 'ant-design-vue/es/notification'
 // 创建 axios 实例
 const request = axios.create({
   // API 请求的默认前缀
-  baseURL: 'http://124.71.4.214:8000/api/',
+  baseURL: 'https://124.71.4.214:8000/api/',
   timeout: 10000 // 请求超时时间
 })
 request.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';//配置请求头信息。
@@ -31,6 +31,7 @@ const errorHandler = (error) => {
       notification.error({
         message: 'Unauthorized',
         description: 'Authorization verification failed'
+
       })
       if (token) {
         // store.dispatch('Logout').then(() => {
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://124.71.4.214:8000/', //接口域名
+                target: 'https://124.71.4.214:8000/', //接口域名
                 changeOrigin: true,             //是否跨域
                 ws: true,                       //是否代理 websockets
                 secure: true,                   //是否https接口
