9692bae3711d3e77b9a526060842d8cb0c1c52b4
zjy <jinyou.zeng@wukongsch.com>
2023-11-08 18:09:10 +0800

    修改没有用户不提示问题

--- src/views/user/createUser.vue
+++ src/views/user/createUser.vue
@@ -3,13 +3,13 @@
     <a-form-item label="name" required>
       <a-input v-model:value="modelRef.username" />
     </a-form-item>
-     <a-form-item label="email" >
+    <a-form-item label="email">
       <a-input v-model:value="modelRef.email" />
     </a-form-item>
-    <a-form-item label="password" required >
+    <a-form-item label="password" required>
       <a-input-password v-model:value="modelRef.password" />
     </a-form-item>
-    
+
     <a-form-item class="error-infos" :wrapper-col="{ span: 14, offset: 4 }">
       <a-button type="primary" @click.prevent="onSubmit">Create</a-button>
       <a-button style="margin-left: 10px" @click="resetFields">Reset</a-button>
@@ -18,70 +18,71 @@
 </template>
 
 <script>
-import { reactive, toRaw, computed, defineComponent,ref } from 'vue';
-import { toArray } from 'lodash-es';
-import { Form } from 'ant-design-vue';
+import { reactive, toRaw, computed, defineComponent, ref } from "vue";
+import { toArray } from "lodash-es";
+import { Form } from "ant-design-vue";
 import { message } from "ant-design-vue";
-import {useRouter} from "vue-router";
-import {user_get,user_api} from '@/api/user'
-
+import { useRouter } from "vue-router";
+import { user_get, user_api } from "@/api/user";
 
 const useForm = Form.useForm;
 export default defineComponent({
-
   setup() {
-         const router = useRouter();
+    const router = useRouter();
 
-      const user = useRouter().currentRoute.value.query
-// console.log(useRouter().currentRoute.value.query.projectid)
-      if (user.id != undefined){
-          user_get(useRouter().currentRoute.value.query).then(res=>{console.log(res)
-          modelRef.username = res.data[0].username,
-           modelRef.email =res.data[0].email,
-           modelRef.password =res.data[0].password,
-           modelRef.id =res.data[0].id
+    const user = useRouter().currentRoute.value.query;
+    // console.log(useRouter().currentRoute.value.query.projectid)
+    if (user.id != undefined) {
+      user_get(useRouter().currentRoute.value.query).then((res) => {
+        console.log(res.code )
+        if (res.code != 200) {
+          message.error({
+            content: res.msg,
+            duration: 5,
           });
+        } else {
+          (modelRef.username = res.data[0].username),
+            (modelRef.email = res.data[0].email),
+            (modelRef.password = res.data[0].password),
+            (modelRef.id = res.data[0].id);
+        }
+      });
     }
 
     const modelRef = reactive({
-      username: '',
-      email: '',
-      password: '',
-        
+      username: "",
+      email: "",
+      password: "",
     });
 
-
-    const {
-      resetFields,
-      validate,
-      validateInfos,
-      mergeValidateInfo,
-    } = useForm(modelRef,);
-
+    const { resetFields, validate, validateInfos, mergeValidateInfo } =
+      useForm(modelRef);
 
     const onSubmit = () => {
-      validate().then(() => {
-        console.log(toRaw(modelRef));
-         var req_method
-        if (user.id == undefined) {
-          req_method = 'post'
-        }else {
-          req_method = 'put'
-        }
-           user_api(modelRef,req_method).then((res) => {
-                message.success({
-                content: res.msg,
-                duration: 5
-                });
-                if (res.code == 200){
-                   router.push("/userList")
-                  // window.location.href="/userList";
-                }
-             console.log(res);
-           });
-      }).catch(err => {
-        console.log('error', err);
-      });
+      validate()
+        .then(() => {
+          console.log(toRaw(modelRef));
+          var req_method;
+          if (user.id == undefined) {
+            req_method = "post";
+          } else {
+            req_method = "put";
+          }
+          user_api(modelRef, req_method).then((res) => {
+            message.success({
+              content: res.msg,
+              duration: 5,
+            });
+            if (res.code == 200) {
+              router.push("/userList");
+              // window.location.href="/userList";
+            }
+            console.log(res);
+          });
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
     };
 
     const errorInfos = computed(() => {
@@ -102,9 +103,7 @@ export default defineComponent({
       value1: ref(),
     };
   },
-
 });
-
 </script>
 <style scoped>
 .error-infos :deep(.ant-form-explain) {
