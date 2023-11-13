5e746d76de8cad530efa6721ae3b532e98d4a6f8
zjy <jinyou.zeng@wukongsch.com>
2023-11-13 18:10:38 +0800

    项目管理代码优化

--- src/views/create_projrct.vue
+++ src/views/create_projrct.vue
@@ -1,20 +1,20 @@
 <template>
-  <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
+  <a-form :label-col="{span: 4}" :wrapper-col="{span: 14}">
     <a-form-item label="name" required>
       <a-input v-model:value="modelRef.name" />
     </a-form-item>
-     <a-form-item label="describe" >
+    <a-form-item label="describe">
       <a-input v-model:value="modelRef.describe" />
     </a-form-item>
     <a-form-item label="start_time" required>
-        <a-space direction="vertical" :size="12" style="float:left">
-          <a-date-picker v-model:value="modelRef.start_time" />
-        </a-space>
+      <a-space direction="vertical" :size="12" style="float: left">
+        <a-date-picker v-model:value="modelRef.start_time" :locale="locale"/>
+      </a-space>
     </a-form-item>
     <a-form-item label="end_time" required>
-       <a-space direction="vertical" :size="12" style="float:left">
-          <a-date-picker v-model:value="modelRef.end_time" />
-       </a-space>
+      <a-space direction="vertical" :size="12" style="float: left">
+        <a-date-picker v-model:value="modelRef.end_time" :locale="locale"/>
+      </a-space>
     </a-form-item>
     <a-form-item class="error-infos" :wrapper-col="{ span: 14, offset: 4 }" v-bind="errorInfos">
       <a-button type="primary" @click.prevent="onSubmit">Create</a-button>
@@ -24,115 +24,76 @@
 </template>
 
 <script>
-import { reactive, toRaw, computed, defineComponent,ref } from 'vue';
-import { toArray } from 'lodash-es';
-import { Form } from 'ant-design-vue';
+import locale from 'ant-design-vue/es/date-picker/locale/zh_CN';
+import { reactive, computed, defineComponent} from "vue";
+import { toArray } from "lodash-es";
+import { Form } from "ant-design-vue";
 import { message } from "ant-design-vue";
-import {useRouter} from "vue-router";
-import {project_get,project_api} from '@/api/project'
-
+import { useRouter } from "vue-router";
+import { project_get, project_api } from "@/api/project";
+import dayjs from 'dayjs';
 
 const useForm = Form.useForm;
 export default defineComponent({
-
   setup() {
-           const router = useRouter();
-
-      const project = useRouter().currentRoute.value.query
-// console.log(useRouter().currentRoute.value.query.projectid)
-      if (useRouter().currentRoute.value.query.id != undefined){
-          project_get(useRouter().currentRoute.value.query).then(res=>{console.log(res)
-          modelRef.name = res.data[0].name,
-           modelRef.describe =res.data[0].describe,
-           modelRef.start_time =res.data[0].start_time,
-           modelRef.end_time =res.data[0].end_time,
-           modelRef.id =res.data[0].id
-          });
-    }
-
-    const modelRef = reactive({
-      name: '',
-      describe: '',
-      start_time: '',
-      end_time: '',
-        id: ''
-    });
-
+    const modelRef = reactive({});
     const rulesRef = reactive({
-      name: [{
-        required: true,
-        message: 'Please input name',
-      }],
-
-      describe: [{
-        message: 'Please input describe',
-      }],
-      start_time: [{
-        required: true,
-        message: 'Please input start_time',
-      }],
-      end_time: [{
-        required: true,
-        message: 'Please input end_time',
-      }],
-
+      name: [{ required: true, message: "Please input name" }],
+      describe: [{ message: "Please input describe" }],
+      start_time: [{ required: true, message: "Please input start_time" }],
+      end_time: [{ required: true, message: "Please input end_time" }],
     });
-    const {
-      resetFields,
-      validate,
-      validateInfos,
-      mergeValidateInfo,
-    } = useForm(modelRef, rulesRef);
-
-
-    const onSubmit = () => {
-      validate().then(() => {
-        console.log(toRaw(modelRef));
-         var req_method
-        if (project.id == undefined) {
-          // console.log('1111111111111111111111',project.id)
-          req_method = 'post'
-        }else {
-          req_method = 'put'
-        }
-           project_api(modelRef,req_method).then((res) => {
-                message.success({
-                content: res.msg,
-                duration: 5
-                });
-                if (res.code == 200){
-                 router.push("/project")
-
-                  // window.location.href="/project";
-                }
-             console.log(res);
-           });
-      }).catch(err => {
-        console.log('error', err);
-      });
-    };
-
+    const { resetFields, validate, validateInfos, mergeValidateInfo } = useForm(
+      modelRef,
+      rulesRef
+    );
     const errorInfos = computed(() => {
       return mergeValidateInfo(toArray(validateInfos));
     });
+
+    const router = useRouter();
+    const isUpdate = router.currentRoute.value.query.id != undefined;
+    if (isUpdate) {
+      project_get(router.currentRoute.value.query).then((res) => {
+        modelRef.id = res.data[0].id;
+        modelRef.name = res.data[0].name;
+        modelRef.describe = res.data[0].describe;
+        modelRef.start_time = dayjs(res.data[0].start_time,'YYYY-MM-DD') ;
+        modelRef.end_time = dayjs(res.data[0].end_time,'YYYY-MM-DD');
+      });
+    }
+
+    const onSubmit = () => {
+      validate()
+        .then(() => {
+          console.log(modelRef);
+          var req_method = isUpdate ? "put" :"post";
+          project_api(modelRef, req_method).then((res) => {
+            message.success({
+              content: res.msg,
+              duration: 5,
+            });
+            if (res.code == 200) {
+              router.push("/project");
+            }
+            console.log(res);
+          });
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
+    };
+
     return {
-      labelCol: {
-        span: 4,
-      },
-      wrapperCol: {
-        span: 14,
-      },
-      validateInfos,
       resetFields,
       modelRef,
       onSubmit,
       errorInfos,
-      value1: ref(),
+      locale,
+      dayjs
     };
   },
-
 });
-
 </script>
 <style scoped>
 .error-infos :deep(.ant-form-explain) {
