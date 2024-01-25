7f7443c43662afb2e9d67573ce95baeba3aaaf5b
“zjy” <jinyou.zeng@wukongsch.com>
2024-01-25 18:05:34 +0800

    修改AI地址

--- src/components/aiChat.vue
+++ src/components/aiChat.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe src="lk-ai.top" style="width: 100%;height: 88vh;"></iframe>
+    <iframe src="https://lk-ai.top/" style="width: 100%;height: 88vh;"></iframe>
 </template>
--- src/components/aiFloating.vue
+++ src/components/aiFloating.vue
@@ -10,7 +10,7 @@
       width ="35%"
       @after-visible-change="afterVisibleChange"
     >
-    <iframe src="lk-ai.top" style="width: 100%;height: 98%;" ></iframe>
+    <iframe src="https://lk-ai.top/" style="width: 100%;height: 98%;" ></iframe>
     </a-drawer>
   </template>
   <script>
